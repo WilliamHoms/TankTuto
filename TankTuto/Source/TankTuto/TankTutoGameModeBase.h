12ec324c13d2add48b7768b221902ef2086753cb
William <therealwilliam@gmail.com>
2019-10-12 08:35:05 -0400

    Initial commit Unreal project

---
+++ TankTuto/Source/TankTuto/TankTutoGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TankTutoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKTUTO_API ATankTutoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
